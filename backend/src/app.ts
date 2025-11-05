484ad03160a369ac0ee4ea227ad08280ff0baddc
Helmi Pirkola <143197627+hpirkola@users.noreply.github.com>
2025-11-05 18:04:27 +0200

    changed error code to 500

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -14,18 +14,18 @@ app.get("/api/dog/image", async (_req: Request, res: Response<DogResponse | Erro
     try {
 		const result = await fetch("https://dog.ceo/api/breeds/image/random");
 		if (!result.ok) {
-			return res.status(502).json({ error: "Failed to fetch image" });
+			return res.status(500).json({ error: "Failed to fetch image" });
 		}
 		const json = (await result.json()) as DogCeoResponse;
 		
 		if (!json || typeof json.message !== "string") {
-			return res.status(502).json({ error: "Upstream returned unexpected data" });
+			return res.status(500).json({ error: "Upstream returned unexpected data" });
 		}
 		const body = { imageUrl: json.message } satisfies DogResponse;
 			return res.json({ imageUrl: json.message });
 	} catch (err: unknown) {
 		console.error(err);
-		return res.status(502).json({ error: "Failed to fetch image" });
+		return res.status(500).json({ error: "Failed to fetch image" });
     }
 });
 
